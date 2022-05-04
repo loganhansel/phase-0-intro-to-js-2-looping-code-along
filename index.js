5026f8d0956922be47cf43df43868c85e800a2c3
loganhansel <loganhansel23@gmail.com>
2022-05-04 18:19:28 -0400

    commit tree 3d3d5dd2fc069419c2194dc29b584628a7a2cc26

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+const thankYouMessages = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        thankYouMessages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+    console.log(n);
+    n--;
+    }
+}
